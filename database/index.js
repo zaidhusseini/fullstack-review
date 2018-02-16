d860e2a4841775caa41f233d5f585529b442a063
zaid <zaidhusseini@gmail.com>
2018-02-16 14:00:11 -0800

    Initialize MongoDB, set up repo schema

--- database/index.js
+++ database/index.js
@@ -3,6 +3,17 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  github_repo_id: Number,
+  github_userid: Number,
+  repo_name: String,
+  user_name: String,
+  stargazer_count: Number,
+  watcher_count: Number,
+  size: Number,
+  created_at: Date,
+  updated_at: Date
+
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
