f9adaf022ecc6f04731d35df74910a35ca38bbcb
zaid <zaidhusseini@gmail.com>
2018-02-17 10:45:03 -0800

    Use Env Variables for API Key

--- helpers/github.js
+++ helpers/github.js
@@ -12,7 +12,7 @@ let getReposByUsername = (username, res) => {
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${process.env.API_KEY}`
     }
   };
 
